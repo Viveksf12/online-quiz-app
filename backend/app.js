216bcce41e27b70b5649eb565c41b938f961c040
Vikash Pandit <panditvikash232@gmail.com>
2026-02-13 13:15:25 +0530

    attempt module connected to server

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const attemptRoutes = require("./attempt");
+app.use("/", attemptRoutes);
+
+
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("../models/User");
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/quizDB")
+.then(()=> console.log("MongoDB connected"))
+.catch(err=> console.log(err));
+
+app.get("/", (req,res)=>{
+  res.send("Server running");
+});
+
+// connect attempt routes
+const attemptRoutes = require("./attempt");
+app.use("/", attemptRoutes);
+
+app.listen(3000, ()=>{
+  console.log("Server started");
+});
