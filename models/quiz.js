8892f612a45406c3e3d752101a9dbd47624cee4a
VIVEK KUMAR NONIA <viveksf532@gmail.com>
2026-02-14 19:56:53 +0000

    Final UI fixes, auth validation, navbar, quiz protection completed

--- models/quiz.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const quizSchema = new mongoose.Schema({
-  title: String,
-  duration: Number,
-  createdBy: String
-});
-
-module.exports = mongoose.model("Quiz", quizSchema);
